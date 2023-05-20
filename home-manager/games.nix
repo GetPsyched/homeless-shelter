fc19b4b3fbab001569f875e5dc9b49066e6c19dd
GetPsyched <priyanshutr@proton.me>
2023-05-20 18:01:15 +0400

    fix: add deps

--- home-manager/games.nix
+++ home-manager/games.nix
@@ -1,5 +1,7 @@
-{pkgs, config, inputs, ...}: {
+{ pkgs, config, inputs, ... }: {
   home.packages = [
+    pkgs.gamemode
     inputs.nix-gaming.packages.${pkgs.system}.osu-stable
+    inputs.nix-gaming.packages.${pkgs.system}.wine-discord-ipc-bridge
   ];
 }
