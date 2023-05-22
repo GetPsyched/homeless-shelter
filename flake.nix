5ecc75a598695921c1dfa3a8a564ab9112e18aab
GetPsyched <priyanshutr@proton.me>
2023-05-23 00:14:45 +0400

    fix: no os but nixos

--- flake.nix
+++ flake.nix
@@ -26,13 +26,5 @@
         }
       ];
     };
-
-    homeConfigurations = {
-      "getpsyched@potato" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home-manager/home.nix ];
-      };
-    };
   };
 }
