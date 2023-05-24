242a457d162adc2869b66f025147bb315f01c367
GetPsyched <priyanshutr@proton.me>
2023-05-24 13:45:39 +0400

    feat: add nifty aliases

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -4,6 +4,11 @@
     userName = "GetPsyched";
     userEmail = "priyanshutr@proton.me";
 
+    aliases = {
+      amend = "commit --amend --no-edit";
+      epic-fail = "reset HEAD~1";
+    };
+
     extraConfig = {
       gpg.format = "ssh";
       user.signingKey = "~/.ssh/id_ed25519.pub";
