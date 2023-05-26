683b682953441cacd1d0a850ec9bb958b62680d3
GetPsyched <priyanshutr@proton.me>
2023-05-26 15:05:24 +0400

    fix: enable bash shell

--- getpsyched/dev/shell.nix
+++ getpsyched/dev/shell.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }: {
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
+  programs = {
+    bash.enable = true;
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
   };
 }
