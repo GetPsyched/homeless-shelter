d895dd80119ee526e540d6b651050d7c6c99d490
GetPsyched <priyanshutr@proton.me>
2023-05-29 11:34:52 +0400

    fix: override wine version

--- getpsyched/games.nix
+++ getpsyched/games.nix
@@ -5,6 +5,6 @@
 
     # Can also use pkgs.osu-lazer-bin
     inputs.nix-gaming.packages.${pkgs.system}.osu-stable
-    inputs.nix-gaming.packages.${pkgs.system}.rocket-league
+    (inputs.nix-gaming.packages.${pkgs.system}.rocket-league.override { wine = inputs.nix-gaming.packages.${pkgs.system}.wine-ge; })
   ];
 }
