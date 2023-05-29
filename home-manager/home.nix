f0891a1b5085bfd62ad23e4fcfb22f851bc45cf3
GetPsyched <priyanshutr@proton.me>
2023-05-29 17:58:56 +0400

    refactor: remove unnecessary config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,26 +7,6 @@
     # ./games.nix
   ];
 
-  nixpkgs = {
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-
-    config = {
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = (_: true);
-    };
-  };
-
   home = {
     username = "getpsyched";
     homeDirectory = "/home/getpsyched";
