a2ed6eeae267aa379c1cb5ec27f77a7400a29d1e
GetPsyched <priyanshutr@proton.me>
2023-06-15 20:17:29 +0400

    shell: use starship

--- getpsyched/dev/shell.nix
+++ getpsyched/dev/shell.nix
@@ -1,10 +1,30 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   programs = {
     bash.enable = true;
 
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+    };
+
+    starship = {
+      enable = true;
+      settings = {
+        format = "$directory$git_branch$character";
+        right_format = lib.strings.concatStrings [
+          "$cmd_duration"
+          "$nix_shell"
+        ];
+        git_branch = {
+          format = "\\([$branch](purple)\\) ";
+        };
+        character = {
+          success_symbol = "[λ](yellow)";
+          error_symbol = "[λ](red)";
+        };
+        directory.style = "cyan";
+        nix_shell.format = "[$symbol](blue)";
+      };
     };
   };
 }
