affab91d846c95cea360fc97a2a3fd2816c5551c
GetPsyched <priyanshutr@proton.me>
2023-06-26 00:26:27 +0400

    gui: banana cursor

--- getpsyched/gui/default.nix
+++ getpsyched/gui/default.nix
@@ -7,6 +7,14 @@
     # ./theme.nix
   ];
 
+  home.pointerCursor = {
+    name = "Banana";
+    size = 48;
+    package = (pkgs.callPackage ../../packages/banana-cursor.nix { });
+    x11.enable = true;
+    gtk.enable = true;
+  };
+
   programs.feh.enable = true;
 
   services.random-background = {
