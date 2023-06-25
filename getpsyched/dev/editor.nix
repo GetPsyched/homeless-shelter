5cb8347a7f9b35465f26bf4e350cae587d9038b4
GetPsyched <priyanshutr@proton.me>
2023-06-26 01:38:50 +0400

    vscode: add user settings

--- getpsyched/dev/editor.nix
+++ getpsyched/dev/editor.nix
@@ -20,6 +20,17 @@
         ms-pyright.pyright
         ms-python.python
       ];
+
+      userSettings = {
+        "diffEditor.renderSideBySide" = false;
+        "editor.inlineSuggest.enabled" = true;
+        "window.menuBarVisibility" = "toggle";
+
+        "[html]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[json]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[python]"."editor.defaultFormatter" = "ms-python.black-formatter";
+        "[nix]"."editor.tabSize" = 2;
+      };
     };
   };
 }
