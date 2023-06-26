001c2634b28c9300e4e8412f8503728d543fc8a8
GetPsyched <priyanshutr@proton.me>
2023-06-26 13:34:18 +0400

    i3: add audio/display keybindings

--- getpsyched/gui/i3.nix
+++ getpsyched/gui/i3.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 let mod = "Mod4"; in
 {
   xsession.windowManager.i3 = {
@@ -11,8 +11,22 @@ let mod = "Mod4"; in
       window.titlebar = false;
       workspaceLayout = "tabbed";
 
-      keybindings = lib.mkOptionDefault {
-        "Print" = "exec flameshot gui";
+      keybindings = 
+        let
+          pactl = "${pkgs.pulseaudio}/bin/pactl";
+          brctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+        in
+        lib.mkOptionDefault {
+          # Audio
+          "XF86AudioRaiseVolume" = "exec ${pactl} set-sink-volume @DEFAULT_SINK@ +10%";
+          "XF86AudioLowerVolume" = "exec ${pactl} set-sink-volume @DEFAULT_SINK@ -10%";
+          "XF86AudioMute" = "exec ${pactl} set-sink-mute @DEFAULT_SINK@ toggle";
+          "XF86AudioMicMute" = "exec ${pactl} set-source-mute @DEFAULT_SOURCE@ toggle";
+
+          # Display
+          "Print" = "exec flameshot gui";
+          "XF86MonBrightnessDown" = "exec ${brctl} set 5%-";
+          "XF86MonBrightnessUp" = "exec ${brctl} set +5%";
       };
     };
   };
