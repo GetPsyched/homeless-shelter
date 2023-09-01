aed0e22ce9860372b72bd9157b655f0f5ae68463
GetPsyched <priyanshutr@proton.me>
2023-09-01 23:11:04 +0530

    fix: persist warp config in correct subvol

--- nixos/impermanence.nix
+++ nixos/impermanence.nix
@@ -9,13 +9,13 @@
   environment.persistence = {
     "/persist/data/system" = {
       directories = [
-        "/var/lib"
       ];
     };
 
     "/persist/state/system" = {
       directories = [
         "/etc/NetworkManager"
+        "/var/lib/cloudflare-warp"
       ];
     };
   };
