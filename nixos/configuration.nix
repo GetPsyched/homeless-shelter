290698a8f6379463d4424fae0846652fe7a69f16
GetPsyched <priyanshutr@proton.me>
2023-09-13 15:00:58 +0530

    revert: idk why I made tis false

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,7 +59,7 @@
   };
 
   # Fix for https://nix-community.github.io/home-manager/index.html#_why_do_i_get_an_error_message_about_literal_ca_desrt_dconf_literal_or_literal_dconf_service_literal
-  programs.dconf.enable = false;
+  programs.dconf.enable = true;
 
   nixpkgs = {
     overlays = [
