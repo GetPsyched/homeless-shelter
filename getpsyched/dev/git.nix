033c837eb0bbd196db0ae9d42d4ffc2830d875b3
GetPsyched <priyanshutr@proton.me>
2023-09-16 19:06:33 +0530

    git: buff config

--- getpsyched/dev/git.nix
+++ getpsyched/dev/git.nix
@@ -8,6 +8,7 @@
 
     aliases = {
       amend = "commit --amend --no-edit";
+      co = "checkout";
       epic-fail = "reset HEAD~1";
     };
 
@@ -18,6 +19,8 @@
       gpg.format = "ssh";
       user.signingKey = "~/.ssh/id_ed25519.pub";
 
+      core.whitespace = "trailing-space,space-before-tab";
+      init.defaultBranch = "master";
       pull.rebase = true;
       push.autoSetupRemote = true;
     };
