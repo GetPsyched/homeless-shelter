127a3596dda6646fe30ad5e0c496b7bf478894b0
GetPsyched <priyanshutr@proton.me>
2023-09-26 01:03:12 +0530

    vscode: add a bunch of language settings

--- getpsyched/dev/editor.nix
+++ getpsyched/dev/editor.nix
@@ -30,11 +30,21 @@
         "explorer.confirmDragAndDrop" = false;
         "window.menuBarVisibility" = "toggle";
 
+        "[css]"."editor.defaultFormatter" = "vscode.css-language-features";
         "[html]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
         "[json]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "[python]"."editor.defaultFormatter" = "ms-python.black-formatter";
         "[nix]"."editor.tabSize" = 2;
-        "[nix]"."editor.defaultFormatter" = "b4dm4n.nixpkgs-fmt";
+        "[nix]"."editor.defaultFormatter" = "jnoortheen.nix-ide";
+        "[python]"."editor.defaultFormatter" = "ms-python.black-formatter";
+        "[svelte]"."editor.defaultFormatter" = "svelte.svelte-vscode";
+        "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[typescriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+
+        "javascript.updateImportsOnFileMove.enabled" = "always";
+        "prettier.singleQuote" = true;
+        "svelte.enable-ts-plugin" = true;
+        "typescript.updateImportsOnFileMove.enabled" = "always";
       };
     };
   };
