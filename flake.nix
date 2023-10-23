1b8823edb1cbcfde2b8af19d3247db5683c4eb34
GetPsyched <priyanshu@getpsyched.dev>
2023-10-23 11:22:28 +0530

    feat: extensions for eww

--- flake.nix
+++ flake.nix
@@ -50,6 +50,18 @@
                 version = "1.0.0";
                 sha256 = "sha256-s60alej3cNAbSJxsRlIRE2Qha6oAsmcOBbWoqp+w6fk=";
               }
+              {
+                name = "vscode-parinfer";
+                publisher = "shaunlebron";
+                version = "0.6.2";
+                sha256 = "sha256-zev0oomPf9B+TaNRnp4xcmEWJBaa+IHgysbX2G0mm0A=";
+              }
+              {
+                name = "yuck";
+                publisher = "eww-yuck";
+                version = "0.0.3";
+                sha256 = "sha256-DITgLedaO0Ifrttu+ZXkiaVA7Ua5RXc4jXQHPYLqrcM=";
+              }
             ];
           })
           just
