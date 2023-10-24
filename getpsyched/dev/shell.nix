4f13890713d0ded78351d962f3416f09b69f30d2
GetPsyched <priyanshu@getpsyched.dev>
2023-10-24 11:16:40 +0530

    direnv: don't timeout

--- getpsyched/dev/shell.nix
+++ getpsyched/dev/shell.nix
@@ -5,6 +5,12 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+
+      config = {
+        # Should be `0s` to disable timeout
+        # See https://github.com/direnv/direnv/issues/1188
+        global.warn_timeout = "69h";
+      };
     };
 
     starship = {
