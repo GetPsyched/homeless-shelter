3796d9cf79b791e8eb503f78a5c7700856ff0ee9
GetPsyched <priyanshu@getpsyched.dev>
2023-10-26 07:01:10 +0530

    refactor: remove comments

--- hosts/carrot/configuration.nix
+++ hosts/carrot/configuration.nix
@@ -35,12 +35,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   users.mutableUsers = false;
@@ -79,16 +73,6 @@
   nixpkgs = {
     overlays = [
       (import ../../overlays/i3.nix)
-
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
   };
 
--- hosts/carrot/hardware-configuration.nix
+++ hosts/carrot/hardware-configuration.nix
@@ -11,13 +11,7 @@
     hostName = "carrot";
     networkmanager.enable = true;
 
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `interfaces.<interface>.useDHCP`.
     useDHCP = lib.mkDefault true;
-    # interfaces.enp44s0.useDHCP = lib.mkDefault true;
-    # interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
   };
 
   boot = {
