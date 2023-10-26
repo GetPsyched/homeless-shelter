f3076601e9f97cdc37003232e4616c79ef034b1c
GetPsyched <priyanshu@getpsyched.dev>
2023-10-26 07:31:40 +0530

    sidebery: remove custom version

--- getpsyched/browser/addons.nix
+++ getpsyched/browser/addons.nix
@@ -1,15 +1,6 @@
 { ff-addons, buildFirefoxXpiAddon }:
 
 ff-addons // {
-  sidebery = buildFirefoxXpiAddon rec {
-    pname = "sidebery";
-    addonId = "{3c078156-979c-498b-8990-85f7987dd929}";
-    version = "5.0.0b31";
-    url =
-      "https://github.com/mbnuqw/sidebery/releases/download/v${version}/sidebery-${version}.xpi";
-    sha256 = "sha256-J7N1w7T421c0B/oZJjpJJ4AsL1YpqUYaAkJsY5IhI+Y=";
-    meta = { };
-  };
   tampermonkey = buildFirefoxXpiAddon rec {
     pname = "tampermonkey";
     addonId = "firefox@tampermonkey.net";
