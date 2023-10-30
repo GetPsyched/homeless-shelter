0acc76e7e3bc3156d579736393b7a8d542a8ef15
GetPsyched <priyanshu@getpsyched.dev>
2023-10-30 22:10:42 +0530

    vscode: refactor: switch to pure nix syntax

--- hosts/piglin/development/editor.nix
+++ hosts/piglin/development/editor.nix
@@ -16,38 +16,46 @@
       package = pkgs.vscodium;
 
       userSettings = {
-        "breadcrumbs.enabled" = false;
-        "diffEditor.renderSideBySide" = false;
-        "editor.formatOnSave" = true;
-        "editor.cursorBlinking" = "expand";
-        "editor.inlineSuggest.enabled" = true;
-        "explorer.confirmDragAndDrop" = false;
-        "search.useGlobalIgnoreFiles" = true;
-        "security.workspace.trust.enabled" = false;
-        "terminal.integrated.cursorBlinking" = true;
-        "terminal.integrated.cursorStyle" = "line";
-        "terminal.integrated.enableMultiLinePasteWarning" = false;
-        "terminal.integrated.enablePersistentSessions" = false;
-        "window.menuBarVisibility" = "toggle";
-        "workbench.panel.opensMaximized" = "always";
-        "workbench.startupEditor" = "none";
+        breadcrumbs.enabled = false;
+        diffEditor.renderSideBySide = false;
+        editor = {
+          cursorBlinking = "expand";
+          formatOnSave = true;
+          inlineSuggest.enabled = true;
+        };
+        explorer.confirmDragAndDrop = false;
+        search.useGlobalIgnoreFiles = true;
+        security.workspace.trust.enabled = false;
+        terminal.integrated = {
+          cursorBlinking = true;
+          cursorStyle = "line";
+          enableMultiLinePasteWarning = false;
+          enablePersistentSessions = false;
+        };
+        window.menuBarVisibility = "toggle";
+        workbench = {
+          panel.opensMaximized = "always";
+          startupEditor = "none";
+        };
 
-        "[css]"."editor.defaultFormatter" = "vscode.css-language-features";
-        "[html]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "[json]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "[jsonc]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "[nix]"."editor.tabSize" = 2;
-        "[nix]"."editor.defaultFormatter" = "jnoortheen.nix-ide";
-        "[python]"."editor.defaultFormatter" = "ms-python.black-formatter";
-        "[svelte]"."editor.defaultFormatter" = "svelte.svelte-vscode";
-        "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-        "[typescriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[css]".editor.defaultFormatter = "vscode.css-language-features";
+        "[html]".editor.defaultFormatter = "esbenp.prettier-vscode";
+        "[javascript]".editor.defaultFormatter = "esbenp.prettier-vscode";
+        "[json]".editor.defaultFormatter = "esbenp.prettier-vscode";
+        "[jsonc]".editor.defaultFormatter = "esbenp.prettier-vscode";
+        "[nix]".editor = {
+          tabSize = 2;
+          defaultFormatter = "jnoortheen.nix-ide";
+        };
+        "[python]".editor.defaultFormatter = "ms-python.black-formatter";
+        "[svelte]".editor.defaultFormatter = "svelte.svelte-vscode";
+        "[typescript]".editor.defaultFormatter = "esbenp.prettier-vscode";
+        "[typescriptreact]".editor.defaultFormatter = "esbenp.prettier-vscode";
 
-        "javascript.updateImportsOnFileMove.enabled" = "always";
-        "prettier.singleQuote" = true;
-        "svelte.enable-ts-plugin" = true;
-        "typescript.updateImportsOnFileMove.enabled" = "always";
+        javascript.updateImportsOnFileMove.enabled = "always";
+        prettier.singleQuote = true;
+        svelte.enable-ts-plugin = true;
+        typescript.updateImportsOnFileMove.enabled = "always";
       };
     };
   };
