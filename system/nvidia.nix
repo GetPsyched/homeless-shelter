89cfa80540b39d8821d4d5225fcecf8e6afd636d
GetPsyched <priyanshu@getpsyched.dev>
2023-10-31 23:37:28 +0530

    nvidia: refactor: move things around

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -1,14 +1,4 @@
 { config, pkgs, ... }:
-
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in
 {
   allowUnfreePackages = [
     "nvidia-settings"
@@ -16,20 +6,31 @@ in
     "nvidia-x11"
   ];
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
+  environment.systemPackages =
+    let
+      nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
+      '';
+    in
+    [ nvidia-offload ];
 
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      prime = {
+        offload.enable = true;
 
-  hardware.nvidia.modesetting.enable = true;
-
-  environment.systemPackages = [ nvidia-offload ];
-
-  hardware.nvidia.prime = {
-    offload.enable = true;
-
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+    opengl.enable = true;
   };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
