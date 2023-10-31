19a1fa64e2feeb84f15a213c3c8801240790ed6e
GetPsyched <priyanshu@getpsyched.dev>
2023-10-31 13:56:00 +0530

    atlauncher: refactor: abstract out themes

--- modules/home-manager/atlauncher.nix
+++ modules/home-manager/atlauncher.nix
@@ -6,17 +6,19 @@ let
   cfg = config.programs.atlauncher;
   jsonFormat = pkgs.formats.json { };
 
-  mergedSettings = cfg.settings
-    // optionalAttrs (cfg.theme == "ATLauncher Light") { "theme" = "com.atlauncher.themes.Light"; }
-    // optionalAttrs (cfg.theme == "Monokai Pro") { "theme" = "com.atlauncher.themes.MonokaiPro"; }
-    // optionalAttrs (cfg.theme == "Dracula Contrast") { "theme" = "com.atlauncher.themes.DraculaContrast"; }
-    // optionalAttrs (cfg.theme == "Hiberbee Dark") { "theme" = "com.atlauncher.themes.HiberbeeDark"; }
-    // optionalAttrs (cfg.theme == "Vuesion") { "theme" = "com.atlauncher.themes.Vuesion"; }
-    // optionalAttrs (cfg.theme == "Material Palenight Contrast") { "theme" = "com.atlauncher.themes.MaterialPalenightContrast"; }
-    // optionalAttrs (cfg.theme == "Arc Orange") { "theme" = "com.atlauncher.themes.ArcOrange"; }
-    // optionalAttrs (cfg.theme == "Cyan Light") { "theme" = "com.atlauncher.themes.CyanLight"; }
-    // optionalAttrs (cfg.theme == "High Tech Darkness") { "theme" = "com.atlauncher.themes.HighTechDarkness"; }
-    // optionalAttrs (cfg.theme == "One Dark") { "theme" = "com.atlauncher.themes.OneDark"; };
+  themeOptions = {
+    "ATLauncher Dark" = "Dark";
+    "ATLauncher Light" = "Light";
+    "Monokai Pro" = "MonokaiPro";
+    "Dracula Contrast" = "DraculaContrast";
+    "Hiberbee Dark" = "HiberbeeDark";
+    "Vuesion" = "Vuesion";
+    "Material Palenight Contrast" = "MaterialPalenightContrast";
+    "Arc Orange" = "ArcOrange";
+    "Cyan Light" = "CyanLight";
+    "High Tech Darkness" = "HighTechDarkness";
+    "One Dark" = "OneDark";
+  };
 in
 {
   meta.maintainers = [ maintainers.getpsyched ];
@@ -43,29 +45,22 @@ in
     };
 
     theme = mkOption {
-      type = types.enum [
-        "ATLauncher Dark"
-        "ATLauncher Light"
-        "Monokai Pro"
-        "Dracula Contrast"
-        "Hiberbee Dark"
-        "Vuesion"
-        "Material Palenight Contrast"
-        "Arc Orange"
-        "Cyan Light"
-        "High Tech Darkness"
-        "One Dark"
-      ];
+      type = types.enum (attrsets.mapAttrsToList (name: value: name) themeOptions);
       default = "ATLauncher Dark";
       example = "One Dark";
       description = "The ATLauncher theme to use.";
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-    xdg.dataFile = mkIf (mergedSettings != { }) {
-      "ATLauncher/configs/ATLauncher.json".source = jsonFormat.generate "atlauncher-config.json" mergedSettings;
+  config =
+    let
+      mergedSettings = cfg.settings
+        // optionalAttrs (cfg.theme != "ATLauncher Dark") { "theme" = "com.atlauncher.themes.${themeOptions.${cfg.theme}}"; };
+    in
+    mkIf cfg.enable {
+      home.packages = [ cfg.package ];
+      xdg.dataFile = mkIf (mergedSettings != { }) {
+        "ATLauncher/configs/ATLauncher.json".source = jsonFormat.generate "atlauncher-config.json" mergedSettings;
+      };
     };
-  };
 }
