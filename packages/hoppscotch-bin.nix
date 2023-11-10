129dfd33b0a39001cc7f72c9912a625ddaf4d1e4
GetPsyched <priyanshu@getpsyched.dev>
2023-11-10 14:56:50 +0530

    feat: hoppscotch

---
+++ packages/hoppscotch-bin.nix
@@ -0,0 +1,53 @@
+{ appimageTools
+, fetchurl
+, lib
+, makeDesktopItem
+}:
+
+let
+  pname = "hoppscotch";
+  version = "23.8.3";
+  name = "${pname}-${version}";
+
+  desktopItem = makeDesktopItem {
+    categories = [ "Development" ];
+    desktopName = "Hoppscotch";
+    exec = pname;
+    icon = pname;
+    name = pname;
+  };
+in
+appimageTools.wrapType1 {
+  inherit name version;
+
+  src = fetchurl {
+    url = "https://github.com/hoppscotch/releases/releases/download/v${version}-1/Hoppscotch_linux_x64.AppImage";
+    hash = "sha256-YqvGYn3TrR4dvx3nKkGRfV7srTllDR4Q+dxttucklUM=";
+  };
+  extraPkgs = pkgs: with pkgs; [ libthai ];
+
+  extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
+
+    mkdir -p $out/share/applications
+    cp -r ${desktopItem} $out/share/applications/${pname}.desktop
+  '';
+
+  meta = with lib; {
+    description = "👽 Open-source API development ecosystem";
+    longDescription = ''
+      Hoppscotch is a lightweight, web-based API development suite. It was built
+      from the ground up with ease of use and accessibility in mind providing
+      all the functionality needed for API developers with minimalist,
+      unobtrusive UI.
+    '';
+    homepage = "https://hoppscotch.com";
+    downloadPage = "https://hoppscotch.com/downloads";
+    changelog = "https://hoppscotch.com/changelog";
+    license = licenses.mit;
+    maintainers = with maintainers; [ getpsyched ];
+    mainProgram = pname;
+    platforms = platforms.all;
+    sourceProvenance = [ sourceTypes.binaryNativeCode ];
+  };
+}
