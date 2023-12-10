225acddfdf39c92931a57ef94f13842675e479a4
GetPsyched <priyanshu@getpsyched.dev>
2023-12-10 13:46:16 +0530

    feat: persist warp tos

--- hosts/piglin/impermanence-home.nix
+++ hosts/piglin/impermanence-home.nix
@@ -4,6 +4,8 @@
     inputs.impermanence.nixosModules.home-manager.impermanence
   ];
 
+  xdg.dataFile."warp/accepted-tos.txt".text = "yes";
+
   home.persistence = {
     "/persist/data/home/getpsyched" = {
       directories = [
