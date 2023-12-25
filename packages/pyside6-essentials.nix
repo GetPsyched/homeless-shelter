2500a99677a0c4339771f12af33f159b7774878f
GetPsyched <priyanshu@getpsyched.dev>
2023-12-25 17:16:06 +0530

    refactor: edit description for pyside6-essentials

--- packages/pyside6-essentials.nix
+++ packages/pyside6-essentials.nix
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   format = "wheel";
 
   src = fetchurl {
-    url = "https://files.pythonhosted.org/packages/e2/f0/09e8e38a51f705eecda1202cb51a9fcc7affb0f9fd3be44bf58ce25528dc/PySide6_Essentials-6.6.0-cp38-abi3-manylinux_2_28_x86_64.whl";
+    url = "https://files.pythonhosted.org/packages/e2/f0/09e8e38a51f705eecda1202cb51a9fcc7affb0f9fd3be44bf58ce25528dc/PySide6_Essentials-${version}-cp38-abi3-manylinux_2_28_x86_64.whl";
     sha256 = "sha256-YChGQWGflk4ctNU88xadejheA3i3Ttt1YQkY0q6hxOU=";
   };
 
@@ -43,7 +43,7 @@ buildPythonPackage rec {
   autoPatchelfIgnoreMissingDeps = [ "libmimerapi.so" ]; # not in nixpkgs yet
 
   meta = with lib; {
-    description = "This is a minimal wheel for PySide6, it includes only the essentials Qt modules";
+    description = "A minimal wheel for PySide6, it includes only the essentials Qt modules";
     homepage = "https://www.pyside.org";
     license = licenses.lgpl3; # unsure which LGPL version
     maintainers = with maintainers; [ getpsyched ];
