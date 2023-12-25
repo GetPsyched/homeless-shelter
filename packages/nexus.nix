1ecbfaf059b9656d78539bb15fb736d6f9627f46
GetPsyched <priyanshu@getpsyched.dev>
2023-12-25 13:42:09 +0530

    nexus: refactor: integrate hacks with build
    I don't know if this is still hacky or this is the correct way to do things. It could be either, but this works.

--- packages/nexus.nix
+++ packages/nexus.nix
@@ -57,32 +57,32 @@ buildPythonApplication rec {
       pyside6-rcc = "${pyside6-essentials}/bin/pyside6-rcc";
       pyside6-lupdate = "${pyside6-essentials}/bin/pyside6-lupdate";
       pyside6-lrelease = "${pyside6-essentials}/bin/pyside6-lrelease";
+
+      sitePackages = "$out/${python.sitePackages}";
+      packageDir = "${sitePackages}/${pname}";
     in
     ''
+      mkdir -p ${packageDir}/{translations,ui}
+
       for file in ui/*.ui; do
         NEW_NAME=$(echo $file | cut --delimiter="." --fields=1).py
-        ${pyside6-uic} $file -o src/nexus/$NEW_NAME
+        ${pyside6-uic} $file -o ${packageDir}/$NEW_NAME
       done
 
-      ${pyside6-rcc} ui/resources.qrc -o src/resources_rc.py
+      mkdir ${sitePackages}/resources_rc
+      ${pyside6-rcc} ui/resources.qrc -o ${sitePackages}/resources_rc/__init__.py
 
       ${pyside6-lupdate} ui/*.ui src/nexus/GUI.py -ts translations/i18n_en.ts
 
-      mkdir -p src/nexus/translations
       for file in translations/*.ts; do
         NEW_NAME=$(echo $file | cut --delimiter="." --fields=1).qm
-        ${pyside6-lrelease} $file -qm src/nexus/$NEW_NAME
+        ${pyside6-lrelease} $file -qm ${packageDir}/$NEW_NAME
       done
     '';
 
   postInstall = ''
     mkdir -p $out/share/icons/hicolor/scalable/apps
     cp ui/images/icon.svg $out/share/icons/hicolor/scalable/apps/${pname}.svg
-
-    # hacks ahead, do not touch
-    mv -v src/nexus/{translations,ui} $out/${python.sitePackages}/${pname}
-    mkdir $out/${python.sitePackages}/resources_rc
-    mv -v src/resources_rc.py $out/${python.sitePackages}/resources_rc/__init__.py
   '';
 
   checkPhase = ''
