c4788e145adcaaf07a1f8464cb6088deec5c713c
GetPsyched <priyanshu@getpsyched.dev>
2023-12-30 15:57:39 +0530

    mgitstatus: make it perfect

--- packages/mgitstatus.nix
+++ packages/mgitstatus.nix
@@ -1,33 +1,32 @@
 { fetchFromGitHub, lib, stdenv }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mgitstatus";
   version = "2.2";
 
   src = fetchFromGitHub {
     owner = "fboender";
     repo = "multi-git-status";
-    rev = version;
+    rev = finalAttrs.version;
     hash = "sha256-jzoX7Efq9+1UdXQdhLRqBlhU3cBrk5AZblg9AYetItg=";
   };
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/bin $out/man/man1
-    install -m 755 mgitstatus $out/bin/${pname}
-    install mgitstatus.1 $out/man/man1
+    install -Dm 755 mgitstatus $out/bin/${finalAttrs.pname}
+    install -Dm 644 mgitstatus.1 $out/share/man/man1/${finalAttrs.pname}.1
 
     runHook postInstall
   '';
 
   meta = with lib; {
     description = "Show uncommitted, untracked and unpushed changes for multiple Git repos";
-    downloadPage = "https://github.com/fboender/multi-git-status/releases/tag/v${version}";
+    downloadPage = "https://github.com/fboender/multi-git-status/releases/tag/v${finalAttrs.version}";
     homepage = "https://github.com/fboender/multi-git-status";
     license = licenses.mit;
     maintainers = [ maintainers.getpsyched ];
+    mainProgram = finalAttrs.pname;
     platforms = platforms.all;
-    sourceProvenance = [ sourceTypes.fromSource ];
   };
-}
+})
