95f23e607664c8b8e9b1ac72e17dae6227ece9b8
GetPsyched <priyanshu@getpsyched.dev>
2023-12-31 18:18:25 +0530

    fluent-runtime: refactor to build from source

--- packages/fluent-runtime.nix
+++ packages/fluent-runtime.nix
@@ -1,26 +1,40 @@
-{ aiohttp, attrs, babel, buildPythonPackage, fetchPypi, fluent-syntax, lib, pythonOlder, pytz }:
+{ buildPythonPackage
+, fetchFromGitHub
+, lib
+, pythonOlder
+, setuptools
+
+, # dependencies
+  attrs
+, babel
+, fluent-syntax
+, pytz
+, typing-extensions
+}:
 
 buildPythonPackage rec {
   pname = "fluent-runtime";
   version = "0.4.0";
-  format = "wheel";
+  format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
-  src = fetchPypi rec {
-    inherit version format;
-    pname = "fluent.runtime";
-    hash = "sha256-Uf0CWCwjY+EQbXBRZClnobf0Bt2cMXvUYApz7eQMUUY=";
-    dist = python;
-    python = "py2.py3";
+  src = fetchFromGitHub {
+    owner = "projectfluent";
+    repo = "python-fluent";
+    rev = "fluent.runtime@${version}";
+    hash = "sha256-Crg6ybweOZ4B3WfLMOcD7+TxGEZPTHJUxr8ItLB4G+Y=";
   };
 
+  setSourceRoot = "sourceRoot=$(echo */fluent.runtime)";
+
   propagatedBuildInputs = [
-    fluent-syntax
-    aiohttp
     attrs
     babel
+    fluent-syntax
     pytz
+    setuptools
+    typing-extensions
   ];
 
   pythonImportsCheck = [
@@ -29,9 +43,11 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Localization library for expressive translations";
-    homepage = "https://github.com/projectfluent/python-fluent";
+    homepage = "https://projectfluent.org/python-fluent";
+    downloadPage = "https://github.com/projectfluent/python-fluent/releases/tag/fluent.runtime@${version}";
     changelog = "https://github.com/projectfluent/python-fluent/blob/main/fluent.runtime/CHANGELOG.rst";
     license = licenses.asl20;
     maintainers = with maintainers; [ getpsyched ];
+    platforms = platforms.all;
   };
 }
