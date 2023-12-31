287f6e15c3aa6e73b5f5c0cf8fdc4d359c4d5d20
GetPsyched <priyanshu@getpsyched.dev>
2023-12-31 22:54:01 +0530

    refactor: make option names plural

--- modules/home-manager/persist.nix
+++ modules/home-manager/persist.nix
@@ -5,27 +5,27 @@ with lib;
 let cfg = config.persist; in
 {
   options.persist = {
-    cacheDir = mkOption { type = with types; listOf str; default = [ ]; };
-    cacheFile = mkOption { type = with types; listOf str; default = [ ]; };
+    cacheDirs = mkOption { type = with types; listOf str; default = [ ]; };
+    cacheFiles = mkOption { type = with types; listOf str; default = [ ]; };
 
-    dataDir = mkOption { type = with types; listOf str; default = [ ]; };
-    dataFile = mkOption { type = with types; listOf str; default = [ ]; };
+    dataDirs = mkOption { type = with types; listOf str; default = [ ]; };
+    dataFiles = mkOption { type = with types; listOf str; default = [ ]; };
 
-    games = mkOption { type = with types; listOf str; default = [ ]; };
+    gameDirs = mkOption { type = with types; listOf str; default = [ ]; };
 
-    stateDir = mkOption { type = with types; listOf str; default = [ ]; };
-    stateFile = mkOption { type = with types; listOf str; default = [ ]; };
+    stateDirs = mkOption { type = with types; listOf str; default = [ ]; };
+    stateFiles = mkOption { type = with types; listOf str; default = [ ]; };
   };
 
   config = let home = config.home.homeDirectory; in {
     home.persistence = {
-      "/var/cache${home}" = { directories = cfg.cacheDir; files = cfg.cacheFile; allowOther = true; };
+      "/var/cache${home}" = { directories = cfg.cacheDirs; files = cfg.cacheFiles; allowOther = true; };
 
-      "/persist/data${home}" = { directories = cfg.dataDir; files = cfg.dataFile; allowOther = true; };
+      "/persist/data${home}" = { directories = cfg.dataDirs; files = cfg.dataFiles; allowOther = true; };
 
-      "/persist/bigdata${home}" = { directories = cfg.games; allowOther = true; };
+      "/persist/bigdata${home}" = { directories = cfg.gameDirs; allowOther = true; };
 
-      "/persist/state${home}" = { directories = cfg.stateDir; files = cfg.stateFile; allowOther = true; };
+      "/persist/state${home}" = { directories = cfg.stateDirs; files = cfg.stateFiles; allowOther = true; };
     };
   };
 
