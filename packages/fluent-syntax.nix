3c9d8181ce2050c304d0832d403e442ff8fc7f0d
GetPsyched <priyanshu@getpsyched.dev>
2023-12-31 18:18:34 +0530

    fluent-syntax: refactor to build from source

--- packages/fluent-syntax.nix
+++ packages/fluent-syntax.nix
@@ -1,29 +1,37 @@
-{ aiohttp, buildPythonPackage, fetchPypi, lib, pythonOlder }:
+{ buildPythonPackage, fetchFromGitHub, lib, pythonOlder, setuptools, typing-extensions }:
 
 buildPythonPackage rec {
   pname = "fluent-syntax";
   version = "0.19.0";
-  format = "wheel";
+  format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
-  src = fetchPypi rec {
-    inherit version format;
-    pname = "fluent.syntax";
-    hash = "sha256-s1KzR1+sbG7V8GUnkh9DKqwHPXZERVCO5SGK7Mx8xcQ=";
-    dist = python;
-    python = "py2.py3";
+  src = fetchFromGitHub {
+    owner = "projectfluent";
+    repo = "python-fluent";
+    rev = "fluent.syntax@${version}";
+    hash = "sha256-nULngwBG/ebICRDi6HMHBdT+r/oq6tbDL7C1iMZpMsA=";
   };
 
+  setSourceRoot = "sourceRoot=$(echo */fluent.syntax)";
+
   propagatedBuildInputs = [
-    aiohttp
+    setuptools
+    typing-extensions
+  ];
+
+  pythonImportsCheck = [
+    "fluent.syntax"
   ];
 
   meta = with lib; {
     description = "Localization library for expressive translations";
-    homepage = "https://github.com/projectfluent/python-fluent";
+    homepage = "https://projectfluent.org/python-fluent";
+    downloadPage = "https://github.com/projectfluent/python-fluent/releases/tag/fluent.syntax@${version}";
     changelog = "https://github.com/projectfluent/python-fluent/blob/main/fluent.syntax/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ getpsyched ];
+    platforms = platforms.all;
   };
 }
