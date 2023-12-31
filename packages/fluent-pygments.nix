d0283cc74b4170f5f82fced961c5b8f985d84ef6
GetPsyched <priyanshu@getpsyched.dev>
2023-12-31 18:18:18 +0530

    fluent-pygments: refactor to build from source

--- packages/fluent-pygments.nix
+++ packages/fluent-pygments.nix
@@ -1,25 +1,37 @@
-{ buildPythonPackage, fetchPypi, lib, pythonOlder }:
+{ buildPythonPackage, fetchFromGitHub, fluent-syntax, lib, pygments, pythonOlder, setuptools }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "fluent-pygments";
-  version = "1.0";
-  format = "wheel";
+  version = "unstable-2021-04-09";
+  format = "setuptools";
 
-  disabled = pythonOlder "3.5";
+  disabled = pythonOlder "3.6";
 
-  src = fetchPypi rec {
-    inherit version format;
-    pname = "fluent.pygments";
-    hash = "sha256-YlyHqKI2LvMEFGsWHTWdz2Ur7Soa5IabVge44G0RfZc=";
-    dist = python;
-    python = "py2.py3";
+  src = fetchFromGitHub {
+    owner = "projectfluent";
+    repo = "python-fluent";
+    rev = "c58681f1e90c14dd36c2ff35d7d487235b685177";
+    hash = "sha256-89kqUlHZzn54M6xm0kB1phCJ+TssFC9wPp+JdS+IGDY=";
   };
+
+  setSourceRoot = "sourceRoot=$(echo */fluent.pygments)";
+
+  propagatedBuildInputs = [
+    fluent-syntax
+    pygments
+    setuptools
+  ];
+
+  pythonImportsCheck = [
+    "fluent.pygments"
+  ];
 
   meta = with lib; {
     description = "Pygments lexer for Fluent";
-    homepage = "https://github.com/projectfluent/python-fluent";
+    homepage = "https://projectfluent.org/python-fluent";
     changelog = "https://github.com/projectfluent/python-fluent/blob/main/fluent.pygments/CHANGELOG.rst";
     license = licenses.asl20;
     maintainers = with maintainers; [ getpsyched ];
+    platforms = platforms.all;
   };
 }
