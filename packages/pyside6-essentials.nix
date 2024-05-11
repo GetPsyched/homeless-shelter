6e0a3b02ba65cf9351e611798fe0054d9d555f60
GetPsyched <priyanshu@getpsyched.dev>
2024-05-11 18:21:57 +0530

    fix: inherit version from shiboken6

--- packages/pyside6-essentials.nix
+++ packages/pyside6-essentials.nix
@@ -15,12 +15,12 @@
 
 buildPythonPackage rec {
   pname = "pyside6-essentials";
-  version = "6.6.0";
+  inherit (shiboken6) version;
   format = "wheel";
 
   src = fetchurl {
-    url = "https://files.pythonhosted.org/packages/e2/f0/09e8e38a51f705eecda1202cb51a9fcc7affb0f9fd3be44bf58ce25528dc/PySide6_Essentials-${version}-cp38-abi3-manylinux_2_28_x86_64.whl";
-    sha256 = "sha256-YChGQWGflk4ctNU88xadejheA3i3Ttt1YQkY0q6hxOU=";
+    url = "https://files.pythonhosted.org/packages/d8/f7/7a8a0c3a87fc9a898a521ae34aea5806f71f7bef1a0e032a6d954550fcea/PySide6_Essentials-${version}-cp39-abi3-manylinux_2_28_x86_64.whl";
+    sha256 = "sha256-4BMjj+QFlrgEBo40rBc1FJQ6Qb+OX7tO38fDCwBDG9U=";
   };
 
   propagatedBuildInputs = [
