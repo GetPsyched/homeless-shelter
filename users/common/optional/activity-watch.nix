1229691dd3203479c2ac2bce7bb325cee5fcb81b
GetPsyched <priyanshu@getpsyched.dev>
2024-05-20 18:06:00 +0530

    feat: launch aw on startup

--- users/common/optional/activity-watch.nix
+++ users/common/optional/activity-watch.nix
@@ -1,7 +1,10 @@
+{ lib, ... }:
 {
   persist.stateDirs = [ ".local/share/activitywatch" ];
 
   services.activitywatch = {
     enable = true;
   };
+  systemd.user.services.activitywatch.Service.Restart = lib.mkForce "always";
+  systemd.user.services.activitywatch-watcher-awatcher.Service.Restart = lib.mkForce "always";
 }
