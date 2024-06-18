5d9b786ac1fd905d69133ecfe53eed13ce81523a
GetPsyched <priyanshu@getpsyched.dev>
2024-06-18 12:24:49 +0400

    style: run nixpkgs-fmt

--- hosts/potato/hardware-configuration.nix
+++ hosts/potato/hardware-configuration.nix
@@ -1,7 +1,8 @@
 { config, lib, modulesPath, ... }:
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot = {
@@ -16,12 +17,14 @@
   };
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/83777c29-90db-478c-8de3-256f22f8584a";
+    {
+      device = "/dev/disk/by-uuid/83777c29-90db-478c-8de3-256f22f8584a";
       fsType = "btrfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7D3C-75DA";
+    {
+      device = "/dev/disk/by-uuid/7D3C-75DA";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
