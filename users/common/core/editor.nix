b48377d6f133aca857d3f291d565e387b0caadba
GetPsyched <priyanshu@getpsyched.dev>
2024-07-03 16:50:27 +0400

    feat: initial helix config

--- users/common/core/editor.nix
+++ users/common/core/editor.nix
@@ -2,9 +2,18 @@
 {
   programs.helix = {
     enable = true;
+    defaultEditor = true;
 
     settings = {
       theme = "tokyonight";
+
+      editor.completion-timeout = 5;
+      editor.preview-completion-insert = false;
+      editor.completion-replace = true;
+      editor.rulers = [ 80 ];
+      editor.lsp.display-inlay-hints = true;
+      editor.cursor-shape.insert = "bar";
+      editor.file-picker.hidden = false;
     };
   };
 
