664836feef49ca1d1f398a66a7080563044efd92
GetPsyched <priyanshu@getpsyched.dev>
2024-09-01 00:14:59 +0530

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'hardware':
        'github:nixos/nixos-hardware/b09c46430ffcf18d575acf5c339b38ac4e1db5d2' (2024-08-19)
      → 'github:nixos/nixos-hardware/95c3dfe6ef2e96ddc1ccdd7194e3cda02ca9a8ef' (2024-08-28)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/2598861031b78aadb4da7269df7ca9ddfc3e1671' (2024-08-18)
      → 'github:nix-community/home-manager/c2cd2a52e02f1dfa1c88f95abeb89298d46023be' (2024-08-23)
    • Updated input 'impermanence':
        'github:nix-community/impermanence/03fe473c731cda2900bae9894b8dfc68e3492db5' (2024-08-20)
      → 'github:nix-community/impermanence/c7f5b394397398c023000cf843986ee2571a1fd7' (2024-08-24)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/c374d94f1536013ca8e92341b540eba4c22f9c62' (2024-08-21)
      → 'github:nixos/nixpkgs/71e91c409d1e654808b2621f28a327acfdad8dc2' (2024-08-28)
    • Updated input 'nixpkgs-master':
        'github:nixos/nixpkgs/e5c918d31bb17310289590f6ecfe051d02bd56ba' (2024-08-22)
      → 'github:nixos/nixpkgs/cd6521160759d5c30f052e3c8a7b2631731cfa12' (2024-08-31)

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -21,6 +21,7 @@
   hardware = {
     nvidia = {
       modesetting.enable = true;
+      open = false;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
       prime = {
         offload.enable = true;
