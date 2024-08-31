a3175f574ca72fcf5fb05f015e244607bbb19fa7
GetPsyched <priyanshu@getpsyched.dev>
2024-08-31 21:29:56 +0530

    feat: use new pr tracker
    It looks uglier, but it's faster

--- users/common/core/firefox/profile-getpsyched/search.nix
+++ users/common/core/firefox/profile-getpsyched/search.nix
@@ -2,12 +2,24 @@
   default = "DuckDuckGo";
   engines =
     let
-      mkEngine = { alias, iconUpdateURL ? "", params ? [ ], url }: {
-        definedAliases = [ alias ];
-        inherit iconUpdateURL;
-        updateInterval = 56 * 24 * 60 * 60 * 1000; # every 8 weeks
-        urls = [{ inherit params; template = url; }];
-      };
+      mkEngine =
+        {
+          alias,
+          iconUpdateURL ? "",
+          params ? [ ],
+          url,
+        }:
+        {
+          definedAliases = [ alias ];
+          inherit iconUpdateURL;
+          updateInterval = 56 * 24 * 60 * 60 * 1000; # every 8 weeks
+          urls = [
+            {
+              inherit params;
+              template = url;
+            }
+          ];
+        };
     in
     {
       "Bing".metaData.hidden = true;
@@ -18,7 +30,12 @@
     // {
       "Home Manager Options" = mkEngine {
         url = "https://home-manager-options.extranix.com";
-        params = [{ name = "query"; value = "{searchTerms}"; }];
+        params = [
+          {
+            name = "query";
+            value = "{searchTerms}";
+          }
+        ];
         iconUpdateURL = "https://home-manager-options.extranix.com/images/favicon.ico";
         alias = "@hmo";
       };
@@ -26,8 +43,14 @@
       "NixOS Options" = mkEngine {
         url = "https://search.nixos.org/options";
         params = [
-          { name = "channel"; value = "unstable"; }
-          { name = "query"; value = "{searchTerms}"; }
+          {
+            name = "channel";
+            value = "unstable";
+          }
+          {
+            name = "query";
+            value = "{searchTerms}";
+          }
         ];
         iconUpdateURL = "https://nixos.org/favicon.ico";
         alias = "@no";
@@ -36,25 +59,45 @@
       "Nix Packages" = mkEngine {
         url = "https://search.nixos.org/packages";
         params = [
-          { name = "channel"; value = "unstable"; }
-          { name = "query"; value = "{searchTerms}"; }
-          { name = "type"; value = "packages"; }
+          {
+            name = "channel";
+            value = "unstable";
+          }
+          {
+            name = "query";
+            value = "{searchTerms}";
+          }
+          {
+            name = "type";
+            value = "packages";
+          }
         ];
         iconUpdateURL = "https://nixos.org/favicon.ico";
         alias = "@np";
       };
 
       "Nix PR Tracker" = mkEngine {
-        url = "https://nixpk.gs/pr-tracker.html";
-        params = [{ name = "pr"; value = "{searchTerms}"; }];
+        url = "https://nixpkgs-tracker.ocfox.me";
+        params = [
+          {
+            name = "pr";
+            value = "{searchTerms}";
+          }
+        ];
         alias = "@pr";
       };
 
       "Source Graph" = mkEngine {
         url = "https://sourcegraph.com/search";
         params = [
-          { name = "q"; value = "context:@maan2003/nix {searchTerms}"; }
-          { name = "groupBy"; value = "repo"; }
+          {
+            name = "q";
+            value = "context:@maan2003/nix {searchTerms}";
+          }
+          {
+            name = "groupBy";
+            value = "repo";
+          }
         ];
         iconUpdateURL = "https://sourcegraph.com/favicon.ico";
         alias = "@sg";
@@ -62,16 +105,30 @@
     }
     # Miscellaneous
     // {
-      "discord.py" = let version = "latest"; in mkEngine {
-        url = "https://discordpy.readthedocs.io/en/${version}/search.html";
-        params = [{ name = "q"; value = "{searchTerms}"; }];
-        iconUpdateURL = "https://discordpy.readthedocs.io/en/stable/_static/discord_py_logo.ico";
-        alias = "@dpy";
-      };
+      "discord.py" =
+        let
+          version = "latest";
+        in
+        mkEngine {
+          url = "https://discordpy.readthedocs.io/en/${version}/search.html";
+          params = [
+            {
+              name = "q";
+              value = "{searchTerms}";
+            }
+          ];
+          iconUpdateURL = "https://discordpy.readthedocs.io/en/stable/_static/discord_py_logo.ico";
+          alias = "@dpy";
+        };
 
       "Library Genesis" = mkEngine {
         url = "https://libgen.rs/search.php";
-        params = [{ name = "req"; value = "{searchTerms}"; }];
+        params = [
+          {
+            name = "req";
+            value = "{searchTerms}";
+          }
+        ];
         iconUpdateURL = "https://libgen.rs/favicon.ico";
         alias = "@libgen";
       };
@@ -84,7 +141,12 @@
 
       "Proton DB" = mkEngine {
         url = "https://www.protondb.com/search";
-        params = [{ name = "q"; value = "{searchTerms}"; }];
+        params = [
+          {
+            name = "q";
+            value = "{searchTerms}";
+          }
+        ];
         alias = "@pdb";
       };
 
@@ -96,9 +158,18 @@
       "Steam DB" = mkEngine {
         url = "https://steamdb.info/search";
         params = [
-          { name = "a"; value = "app"; }
-          { name = "type"; value = "1"; }
-          { name = "q"; value = "{searchTerms}"; }
+          {
+            name = "a";
+            value = "app";
+          }
+          {
+            name = "type";
+            value = "1";
+          }
+          {
+            name = "q";
+            value = "{searchTerms}";
+          }
         ];
         alias = "@sdb";
       };
