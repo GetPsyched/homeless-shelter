bf9515c65aaab2aa98fc096a6cbf65aa6aad97d9
GetPsyched <priyanshu@getpsyched.dev>
2024-09-08 17:52:39 +0530

    refactor(wip): remove remains of i3
    goldfish derivation will now break because I haven't setup cosmic there yet

--- overlays/i3.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  i3 = prev.i3.overrideAttrs (drv: {
-    patches = drv.patches or [ ] ++ [
-      ./i3-hide-titlebar-in-tabbed-and-stacking-modes.patch
-    ];
-  });
-}
--- users/common/core/tools/default.nix
+++ users/common/core/tools/default.nix
@@ -1,69 +1,26 @@
 { pkgs, ... }:
 {
-  imports = [
-    ./rofi
-  ];
+  home.packages = with pkgs; [ feh ];
 
-  home.packages = with pkgs; [
-    feh
-    peek
-  ];
+  services.flameshot = {
+    enable = true;
 
-  services = {
-    dunst = {
-      enable = true;
-      settings = {
-        global = {
-          font = "Roboto Mono 8";
-          width = 300;
-          origin = "top-right";
-          offset = "40x50";
-          transparency = 0;
-          frame_width = 2;
-          separator_height = 2;
-          sort = true;
-        };
-        urgency_low = {
-          background = "#191919";
-          foreground = "#BBBBBB";
-        };
-        urgency_normal = {
-          background = "#191919";
-          foreground = "#BBBBBB";
-        };
-        urgency_critical = {
-          background = "#191919";
-          foreground = "#DE6E7C";
-        };
+    settings = {
+      General = {
+        disabledTrayIcon = true;
+        saveLastRegion = true;
+        showDesktopNotification = false;
+        showStartupLaunchMessage = false;
+        uploadWithoutConfirmation = true;
       };
     };
+  };
 
-    flameshot = {
-      enable = true;
-
-      settings = {
-        General = {
-          disabledTrayIcon = true;
-          saveLastRegion = true;
-          showDesktopNotification = false;
-          showStartupLaunchMessage = false;
-          uploadWithoutConfirmation = true;
-        };
-      };
-    };
-
-    gammastep = {
-      enable = true;
-      temperature.day = 4500;
-      temperature.night = 4500;
-      duskTime = "18:35-20:15";
-      dawnTime = "6:00-7:45";
-    };
-
-    random-background = {
-      enable = true;
-      imageDirectory = "%h/backgrounds";
-      interval = "1h";
-    };
+  services.gammastep = {
+    enable = true;
+    temperature.day = 4500;
+    temperature.night = 4500;
+    duskTime = "18:35-20:15";
+    dawnTime = "6:00-7:45";
   };
 }
