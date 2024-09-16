b7e274b77c74d46c315e4041afafe37dc51b9983
GetPsyched <priyanshu@getpsyched.dev>
2024-09-16 15:58:18 +0530

    feat: zed assistant
    this is state, not config :(

--- users/common/optional/zed.nix
+++ users/common/optional/zed.nix
@@ -5,6 +5,15 @@
     enable = true;
 
     settings = {
+      assistant = {
+        version = "1";
+        provider = {
+          default_model = "claude-3-5-sonnet";
+          name = "anthropic";
+          low_speed_timeout_in_seconds = 60;
+        };
+      };
+
       theme = "Ayu Dark";
     };
   };
