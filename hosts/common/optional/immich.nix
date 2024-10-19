4433b4bad72d6884538a8a30cae2406543690f67
GetPsyched <priyanshu@getpsyched.dev>
2024-10-19 15:42:20 +0530

    feat: immich

---
+++ hosts/common/optional/immich.nix
@@ -0,0 +1,6 @@
+{
+  services.immich = {
+    enable = true;
+    host = "0.0.0.0";
+  };
+}
--- hosts/fledgeling/default.nix
+++ hosts/fledgeling/default.nix
@@ -1,5 +1,13 @@
+{ config, ... }:
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [
+    ../common/core
+    ./hardware-configuration.nix
+
+    ../common/optional/immich.nix
+  ];
+
+  users.users.root.hashedPassword = "$y$j9T$xg0X2wgh7cke3y.42NiDw0$VQoyREzlRwxySHRSW1Sfi.cDUhjoPNVLS76MDes01o6";
 
   documentation.nixos.enable = false;
 
@@ -7,6 +15,14 @@
     80
     443
   ];
+
+  services.caddy.enable = true;
+  services.caddy.virtualHosts = {
+    "immich.getpsyched.dev".extraConfig = ''
+      encode zstd gzip
+      reverse_proxy localhost:${toString config.services.immich.port}
+    '';
+  };
 
   time.timeZone = "Europe/Berlin";
   system.stateVersion = "24.05";
