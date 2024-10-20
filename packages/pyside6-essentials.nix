e21c03c2e2155094e8a50719c70337db68406765
GetPsyched <priyanshu@getpsyched.dev>
2024-10-20 11:16:10 +0530

    pyside6-essentials: modernise

--- packages/pyside6-essentials.nix
+++ packages/pyside6-essentials.nix
@@ -1,31 +1,28 @@
-{ autoPatchelfHook
-, buildPythonPackage
-, fetchurl
-, lib
-, shiboken6
+{
+  autoPatchelfHook,
+  buildPythonPackage,
+  fetchurl,
+  lib,
+  shiboken6,
 
   # libraries
-, gtk3
-, libxkbcommon
-, mysql80
-, postgresql
-, qt6
-, unixODBC
+  gtk3,
+  libxkbcommon,
+  mysql80,
+  postgresql,
+  qt6,
+  unixODBC,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "pyside6-essentials";
   inherit (shiboken6) version;
   format = "wheel";
 
   src = fetchurl {
-    url = "https://files.pythonhosted.org/packages/9d/fd/17510a0abd503a904ce3b9f1af87385435cf9340fb79c020a53d3a8385a5/PySide6_Essentials-${version}.0-cp39-abi3-manylinux_2_28_x86_64.whl";
+    url = "https://files.pythonhosted.org/packages/9d/fd/17510a0abd503a904ce3b9f1af87385435cf9340fb79c020a53d3a8385a5/PySide6_Essentials-${shiboken6.version}.0-cp39-abi3-manylinux_2_28_x86_64.whl";
     sha256 = "sha256-2pmpSAZBbsHjhkJqR059HlFMHN+K0XHABTdvT2M+chY=";
   };
-
-  propagatedBuildInputs = [
-    shiboken6
-  ];
 
   nativeBuildInputs = [
     autoPatchelfHook
@@ -40,12 +37,19 @@ buildPythonPackage rec {
     unixODBC # libodbc.so.2
   ];
 
-  autoPatchelfIgnoreMissingDeps = [ "libmimerapi.so" "libQt6EglFsKmsGbmSupport.so.6" ]; # not in nixpkgs yet
+  dependencies = [
+    shiboken6
+  ];
 
-  meta = with lib; {
-    description = "A minimal wheel for PySide6, it includes only the essentials Qt modules";
+  autoPatchelfIgnoreMissingDeps = [
+    "libmimerapi.so"
+    "libQt6EglFsKmsGbmSupport.so.6"
+  ]; # not in nixpkgs yet
+
+  meta = {
+    description = "Minimal wheel for PySide6, it includes only the essentials Qt modules";
     homepage = "https://www.pyside.org";
-    license = licenses.lgpl3; # unsure which LGPL version
-    maintainers = with maintainers; [ getpsyched ];
+    license = lib.licenses.lgpl3; # unsure which LGPL version
+    maintainers = with lib.maintainers; [ getpsyched ];
   };
 }
