92c1a2e526d66fcdc404dab1ab857bfccf4f9e78
GetPsyched <priyanshu@getpsyched.dev>
2024-11-18 23:04:29 +0400

    fix: don't auto gc

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -6,12 +6,6 @@
 }:
 {
   nix = {
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-
     # This will additionally add your inputs to the system's legacy channels
     # Making legacy nix commands consistent as well, awesome!
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
