6823695626a313b9543dc3d42465c26728c61ab6
GetPsyched <priyanshu@getpsyched.dev>
2024-11-22 19:38:41 +0400

    fix: i3 needs tray too

--- config/i3.nix
+++ config/i3.nix
@@ -66,6 +66,14 @@ in
       };
     };
 
+    # Needed by apps like Flameshot
+    systemd.user.targets.tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = [ "graphical-session-pre.target" ];
+      };
+    };
+
     services.dunst = {
       enable = true;
       settings = {
