8ab1e88bdf1169eafe65e3d6caeb053801507375
GetPsyched <priyanshu@getpsyched.dev>
2025-01-24 02:13:14 +0530

    refactor: move home to its own file

---
+++ config/home.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  inputs,
+  outputs,
+  pkgs,
+  ...
+}:
+{
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit inputs outputs;
+    };
+    sharedModules = [ (import ../modules/home-manager) ];
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+
+  home-manager.users.${config.mainuser} = {
+    home = {
+      username = config.mainuser;
+      homeDirectory = config.users.users.${config.mainuser}.home;
+      stateVersion = "22.11";
+
+      pointerCursor = {
+        name = "Banana";
+        size = 48;
+        package = pkgs.banana-cursor;
+        x11.enable = true;
+        gtk.enable = true;
+      };
+    };
+
+    # Nicely reload system units when changing configs
+    systemd.user.startServices = "sd-switch";
+  };
+}
--- config/user.nix
+++ config/user.nix
@@ -1,51 +1,18 @@
+{ config, ... }:
 {
-  config,
-  inputs,
-  lib,
-  outputs,
-  pkgs,
-  ...
-}:
-{
+  imports = [ ./home.nix ];
+
+  persist.cache.homeDirectories = [ ".cache" ];
+
   users.mutableUsers = false;
   users.users.${config.mainuser} = {
-    isNormalUser = true;
     description = "Priyanshu Tripathi";
     extraGroups = [
       "networkmanager"
       "wheel"
     ];
     hashedPassword = "$y$j9T$KIZivxYTTPjQKqXxXhGRR/$ATU7co5bqgYl2rzHk9xPf5sgflqhGykTEClGx2jAiM2";
+    home = "/home/${config.mainuser}";
+    isNormalUser = true;
   };
-
-  imports = [ inputs.home-manager.nixosModules.home-manager ];
-
-  home-manager = {
-    extraSpecialArgs = {
-      inherit inputs outputs;
-    };
-    sharedModules = [ (import ../modules/home-manager) ];
-    useGlobalPkgs = true;
-    useUserPackages = true;
-  };
-
-  home-manager.users.${config.mainuser} = {
-    home = {
-      username = config.mainuser;
-      homeDirectory = lib.mkDefault "/home/${config.home.username}";
-      stateVersion = "22.11";
-
-      pointerCursor = {
-        name = "Banana";
-        size = 48;
-        package = pkgs.banana-cursor;
-        x11.enable = true;
-        gtk.enable = true;
-      };
-    };
-
-    # Nicely reload system units when changing configs
-    systemd.user.startServices = "sd-switch";
-  };
-  persist.cache.homeDirectories = [ ".cache" ];
 }
