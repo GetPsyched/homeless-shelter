ce99d181b467814a7327a5eed75a5c5b919789b5
GetPsyched <priyanshu@getpsyched.dev>
2025-02-15 19:02:53 +0700

    fix: add correct builder config

--- config/builders.nix
+++ config/builders.nix
@@ -6,12 +6,40 @@
   nix.buildMachines = [
     {
       hostName = "build-box.nix-community.org";
+      maxJobs = 12;
       protocol = "ssh-ng";
       publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUVsSVE1NHFBeTdEaDYzckJ1ZFlLZGJ6SkhycmJyck1YTFlsN1BrbWs4OEg=";
       sshKey = "${config.users.users.${config.mainuser}.home}/.ssh/id_ed25519";
       sshUser = config.mainuser;
-      supportedFeatures = [ "big-parallel" ];
-      systems = [ "x86_64-linux" ];
+      supportedFeatures = [
+        "benchmark"
+        "big-parallel"
+        "gccarch-broadwell"
+        "gccarch-haswell"
+        "gccarch-ivybridge"
+        "gccarch-nehalem"
+        "gccarch-sandybridge"
+        "gccarch-skylake"
+        "gccarch-westmere"
+        "gccarch-x86-64"
+        "gccarch-x86-64-v2"
+        "gccarch-x86-64-v3"
+        "gccarch-znver1"
+        "gccarch-znver2"
+        "kvm"
+        "nixos-test"
+        "uid-range"
+      ];
+      systems = [
+        "x86_64-linux"
+
+        # extra-platforms
+        "i686-linux"
+        "riscv64-linux"
+        "x86_64-v1-linux"
+        "x86_64-v2-linux"
+        "x86_64-v3-linux"
+      ];
     }
   ];
 }
