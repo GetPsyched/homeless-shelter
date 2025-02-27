02587cd6b9696763cb7b7097c06a7cb342445a88
GetPsyched <priyanshu@getpsyched.dev>
2025-02-27 12:17:08 +0530

    refactor: install ungoogled-chromium normally

--- config/firefox/default.nix
+++ config/firefox/default.nix
@@ -1,32 +1,25 @@
 { lib, pkgs, ... }:
 {
-  home-manager.users.primary = {
-    programs.firefox = {
-      enable = true;
-      profiles.main =
-        let
-          profile = ./profile-primary;
-        in
-        lib.recursiveUpdate {
-          isDefault = true;
+  home-manager.users.primary.programs.firefox = {
+    enable = true;
+    profiles.main =
+      let
+        profile = ./profile-primary;
+      in
+      lib.recursiveUpdate {
+        isDefault = true;
 
-          settings = {
-            "browser.startup.page" = 3; # restore
-            "distribution.searchplugins.defaultLocale" = "en-GB";
-            "drm" = true;
-            "general.useragent.locale" = "en-GB";
-          };
-        } (lib.optionalAttrs (builtins.pathExists profile) (import profile { inherit lib pkgs; }));
-    };
-
-    xdg.desktopEntries.chromium = {
-      categories = [ "Application" ];
-      name = "Chromium";
-      exec = "${pkgs.ungoogled-chromium}/bin/chromium";
-      genericName = "Browser";
-      icon = "${pkgs.ungoogled-chromium}/share/icons/hicolor/48x48/apps/chromium.png";
-      terminal = false;
-    };
+        settings = {
+          "browser.startup.page" = 3; # restore
+          "distribution.searchplugins.defaultLocale" = "en-GB";
+          "drm" = true;
+          "general.useragent.locale" = "en-GB";
+        };
+      } (lib.optionalAttrs (builtins.pathExists profile) (import profile { inherit lib pkgs; }));
   };
+
   persist.state.homeDirectories = [ ".mozilla/firefox/main" ];
+
+  # just in case
+  users.users.primary.packages = [ pkgs.ungoogled-chromium ];
 }
