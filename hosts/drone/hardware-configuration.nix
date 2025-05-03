9e2b3f99f1955cec033ce18eef1dd4e34cd3b168
GetPsyched <priyanshu@getpsyched.dev>
2025-05-03 18:36:19 +0530

    nvme setup

--- hosts/drone/hardware-configuration.nix
+++ hosts/drone/hardware-configuration.nix
@@ -1,8 +1,20 @@
+{ config, modulesPath, ... }:
 {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules = [ "nvme" ];
+
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-    fsType = "ext4";
+    device = "/dev/disk/by-uuid/cdb97443-8361-4f5a-a9a8-eb25885b1b4f";
+    fsType = "btrfs";
   };
 
-  nixpkgs.buildPlatform = "x86_64-linux";
+  fileSystems.${config.boot.loader.efi.efiSysMountPoint} = {
+    device = "/dev/disk/by-uuid/F323-FA4E";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
 }
