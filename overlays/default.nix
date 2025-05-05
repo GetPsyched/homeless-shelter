1fda1fcc4e0fc4c788036857ae83fc08591b35e1
GetPsyched <priyanshu@getpsyched.dev>
2025-05-05 06:58:30 +0530

    cleanup: make firefox overlay look pretty

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,24 +4,26 @@
 
   modifications = final: prev: {
     firefox = prev.firefox.overrideAttrs (old: {
-      buildCommand =
-        ''
-          export buildRoot="$(pwd)"
-        ''
-        + old.buildCommand
-        + ''
-          pushd $buildRoot
-          ${lib.getExe prev.unzip} $out/lib/firefox/browser/omni.ja -d patched_omni || ret=$?
-          if [[ $ret && $ret -ne 2 ]]; then
-            echo "unzip exited with unexpected error"
-            exit $ret
-          fi
-          rm $out/lib/firefox/browser/omni.ja
-          cd patched_omni
-          sed -i 's/"enterprise_only"\s*:\s*true,//' modules/policies/schema.sys.mjs
-          ${lib.getExe prev.zip} -0DXqr $out/lib/firefox/browser/omni.ja * # potentially qr9XD
-          popd
-        '';
+      buildCommand = ''
+        export buildRoot="$(pwd)"
+
+        ${old.buildCommand}
+
+        pushd $buildRoot
+        ${lib.getExe prev.unzip} $out/lib/firefox/browser/omni.ja -d patched_omni || ret=$?
+        if [[ $ret && $ret -ne 2 ]]; then
+          echo "unzip exited with unexpected error"
+          exit $ret
+        fi
+        rm $out/lib/firefox/browser/omni.ja
+
+        sed -i 's/"enterprise_only"\s*:\s*true,//' patched_omni/modules/policies/schema.sys.mjs
+
+        cd patched_omni
+        ${lib.getExe prev.zip} -0 --recurse-paths --no-dir-entries -X --quiet $out/lib/firefox/browser/omni.ja *
+        cd .. && rm -r patched_omni
+        popd
+      '';
     });
   };
 }
