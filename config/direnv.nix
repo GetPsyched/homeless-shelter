49ea6656f5c71b9438051da9f411bcc68512e5f8
GetPsyched <priyanshu@getpsyched.dev>
2025-06-07 02:11:56 +0530

    direnv: save cache into ~/.cache, not .direnv

--- config/direnv.nix
+++ config/direnv.nix
@@ -9,11 +9,20 @@
         global.warn_timeout = "0s";
         whitelist.prefix = [ "~/src" ];
       };
-    };
 
-    programs.git.ignores = [
-      ".direnv"
-      ".envrc"
-    ];
+      stdlib = ''
+        : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+          local hash path
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+            hash="$(sha1sum - <<< "$PWD" | head -c40)"
+            path="''${PWD//[^a-zA-Z0-9]/-}"
+            echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+          )}"
+        }
+      '';
+    };
   };
+  persist.cache.homeDirectories = [ ".cache/direnv/layouts" ];
 }
