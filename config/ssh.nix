442f6860c20a7e784ddfeee7a09c717eddb040fa
GetPsyched <priyanshu@getpsyched.dev>
2025-06-14 14:11:43 +0530

    ssh: add known hosts

--- config/ssh.nix
+++ config/ssh.nix
@@ -1,4 +1,9 @@
 {
+  programs.ssh.knownHosts = {
+    fledgeling.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRAnt3jVeTeAPgX8ZUkSWxfGJas2iMbr9ggLyM9gqZ8";
+    piglin.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAMOdZlxk+2X1nn0i6coQGyXBv+vPxfm0yPGAZ9srSUU";
+  };
+
   services.openssh = {
     enable = true;
 
