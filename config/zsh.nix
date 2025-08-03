713ad66ca9b972d03c135ea8b4071874374b7d7f
GetPsyched <priyanshu@getpsyched.dev>
2025-08-03 21:11:52 +0530

    zsh: add zmv

--- config/zsh.nix
+++ config/zsh.nix
@@ -5,12 +5,16 @@
 
   home-manager.users.primary.programs.zsh = {
     enable = true;
+
     autosuggestion.enable = true;
     history = {
       extended = true;
       ignoreAllDups = true;
       path = "${config.users.users.primary.home}/.local/state/zsh/history";
     };
+    initContent = ''
+      autoload -Uz zmv
+    '';
   };
 
   persist.state.homeDirectories = [ ".local/state/zsh" ];
