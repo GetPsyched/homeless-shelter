e060efbd6a2fc133d5d929d19335e6e8ed762b37
GetPsyched <priyanshu@getpsyched.dev>
2025-11-21 03:25:37 +0530

    fixup: add banana cursor

--- config/home.nix
+++ config/home.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.hjem.nixosModules.default ];
 
@@ -7,4 +7,12 @@
     extraModules = [ inputs.hjem-rum.hjemModules.default ];
     # linker = inputs.hjem.packages.smfh;
   };
+
+  hjem.users.primary.xdg.data.files."icons/Banana".source =
+    "${pkgs.banana-cursor}/share/icons/Banana";
+  # hjem.users.primary.rum.misc.gtk.settings.cursor-theme-name = "Banana";
+  hjem.users.primary.files.".Xresources".text = ''
+    Xcursor.theme: Banana
+    Xcursor.size: 48
+  '';
 }
