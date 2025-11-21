727946de458172ddc826ee65a75ff41631b0acc5
GetPsyched <priyanshu@getpsyched.dev>
2025-11-21 10:57:00 +0530

    firefox: drop overlay
    Support for configuring SearchEngines was added to all versions of Firefox in v139

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 {
   additions = final: _prev: import ../packages { pkgs = final; };
 
@@ -8,29 +8,6 @@
         inherit (final) config;
         inherit (final.stdenv.hostPlatform) system;
       }).citrix_workspace;
-
-    firefox = prev.firefox.overrideAttrs (old: {
-      buildCommand = ''
-        export buildRoot="$(pwd)"
-
-        ${old.buildCommand}
-
-        pushd $buildRoot
-        ${lib.getExe prev.unzip} $out/lib/firefox/browser/omni.ja -d patched_omni || ret=$?
-        if [[ $ret && $ret -ne 2 ]]; then
-          echo "unzip exited with unexpected error"
-          exit $ret
-        fi
-        rm $out/lib/firefox/browser/omni.ja
-
-        sed -i 's/"enterprise_only"\s*:\s*true,//' patched_omni/modules/policies/schema.sys.mjs
-
-        cd patched_omni
-        ${lib.getExe prev.zip} -0 --recurse-paths --no-dir-entries -X --quiet $out/lib/firefox/browser/omni.ja *
-        cd .. && rm -r patched_omni
-        popd
-      '';
-    });
 
     # Pin to fix winboat
     # https://github.com/NixOS/nixpkgs/issues/462513
