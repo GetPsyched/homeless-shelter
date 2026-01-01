53e6a4eb0360e853f715ebfa1338d8f4f3ba8740
GetPsyched <priyanshu@getpsyched.dev>
2026-01-01 07:48:42 +0530

    helix: resolve nixfmt alias

--- config/helix.nix
+++ config/helix.nix
@@ -18,7 +18,7 @@
       {
         name = "nix";
         auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        formatter.command = lib.getExe pkgs.nixfmt;
         language-servers = [ "nixd" ];
       }
       {
