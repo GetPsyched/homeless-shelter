f60b8dbfdabf8fcdecd081b373554f2e950aba7f
GetPsyched <priyanshu@getpsyched.dev>
2026-05-16 17:32:46 +0530

    tailscale: set auth key

--- config/tailscale.nix
+++ config/tailscale.nix
@@ -1,10 +1,19 @@
-{ config, ... }:
+{ config, inputs, ... }:
 {
   services.tailscale = {
     enable = true;
     openFirewall = true;
+
+    authKeyFile = config.age.secrets.tailscale.path;
+    extraUpFlags = [ "--ssh" ];
   };
   persist.state.directories = [ "/var/lib/tailscale" ];
 
   networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName ];
+
+  age.secrets.tailscale = {
+    file = "${inputs.self}/secrets/tailscale.age";
+    owner = config.users.users.primary.name;
+    group = config.users.users.primary.group; # https://github.com/ryantm/agenix/issues/374
+  };
 }
