d2b5caa42847215fa35c479a7045669c8b4116b4
GetPsyched <priyanshu@getpsyched.dev>
2026-05-16 17:55:22 +0530

    restic: backup host data and state

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,5 +4,8 @@ let
   keys = concatLists (attrValues (import ../keys.nix).users) ++ attrValues (import ../keys.nix).hosts;
 in
 {
+  "restic/env.age".publicKeys = keys;
+  "restic/pass.age".publicKeys = keys;
+
   "tailscale.age".publicKeys = keys;
 }
