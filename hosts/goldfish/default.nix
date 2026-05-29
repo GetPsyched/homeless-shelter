2cc71d27518e7d1d853fccb5181a4055244a3172
GetPsyched <priyanshu@getpsyched.dev>
2026-05-29 11:17:55 +0530

    goldfish: present as ephemeral to tailscale

--- hosts/goldfish/default.nix
+++ hosts/goldfish/default.nix
@@ -1,5 +1,6 @@
 {
   hostName,
+  inputs,
   lib,
   pkgs,
   ...
@@ -26,6 +27,8 @@
       configurationName = hostName;
       volumeID = hostName;
     };
+
+    age.secrets.tailscale.file = lib.mkForce "${inputs.self}/secrets/tailscale-ephemeral.age";
 
     environment.systemPackages = [ pkgs.gparted ];
     users.users.primary.packages = [ pkgs.wifi-qr ];
