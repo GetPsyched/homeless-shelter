0d8684fa561e635ab6c0b4617dfaeae3ac00719a
GetPsyched <priyanshu@getpsyched.dev>
2026-06-08 15:47:43 +0530

    dock hide?

--- hosts/mitsuko/default.nix
+++ hosts/mitsuko/default.nix
@@ -20,6 +20,10 @@
     pkgs.firefox
   ];
 
+  system.defaults = {
+    dock.autohide = true;
+  };
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 6;
