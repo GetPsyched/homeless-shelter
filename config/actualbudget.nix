ef57a58d2330165762512d14a1480f369af6b396
GetPsyched <priyanshu@getpsyched.dev>
2026-08-26 00:49:59 +0530

    actualbudget: serve via tailscale

--- config/actualbudget.nix
+++ config/actualbudget.nix
@@ -13,4 +13,33 @@
   };
 
   persist.data.directories = [ config.services.actual.settings.dataDir ];
+
+  # FIXME https://github.com/tailscale/tailscale/issues/18381
+  # services.tailscale.serve.enable = true;
+  # services.tailscale.serve.services.actual = {
+  #   endpoints."tcp:443" = "http://${config.services.actual.settings.hostname}:${toString config.services.actual.settings.port}";
+  # };
+
+  systemd.services.actual-tailscale-serve = {
+    description = "Tailscale Service proxy for ActualBudget";
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "actual.service"
+      "tailscaled.service"
+    ];
+    requires = [ "tailscaled.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+
+      ExecStart = builtins.concatStringsSep " " [
+        "${config.services.tailscale.package}/bin/tailscale serve"
+        "--service=svc:actual"
+        "--https=443"
+        "http://localhost:${toString config.services.actual.settings.port}"
+      ];
+      ExecStop = "${config.services.tailscale.package}/bin/tailscale serve clear svc:actual";
+    };
+  };
 }
